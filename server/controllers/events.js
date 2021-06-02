e1af47e60f8dc908dcfee9d1a48ec35414cf46fc
Jon Hourcade <jonhourcade@Jons-Mac-mini.local>
2021-06-02 09:15:08 -0500

    about to pull from upstream

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -3,7 +3,23 @@ const { wrapAsync } = require('../helpers');
 
 const getAllEvents = wrapAsync(async (req, res) => {
   const events = await Event.find();
-  res.send(events);
+  const mappedEvents = await Promise.all(
+    events.map(async (event) => {
+      const { firstName, lastName } = await User.findById(event.owner);
+      const mappedAttendees = await Promise.all(
+        event.attendees.map(async (attendeeId) => {
+          const attendeeObj = await User.findById(attendeeId);
+          return `${attendeeObj.firstName} ${attendeeObj.lastName}`;
+        })
+      );
+      return {
+        ...event._doc, // why ???
+        owner: `${firstName} ${lastName}`,
+        attendees: mappedAttendees,
+      };
+    })
+  );
+  res.send(mappedEvents);
 });
 
 const getUserEvents = wrapAsync(async (req, res) => {
@@ -17,6 +33,7 @@ const addNewEvent = wrapAsync(async (req, res) => {
   const { userId } = req.params;
   const { eventName, locationName, lat, lng, time, description, isPublic } =
     req.body;
+  const user = await User.findById(userId);
   const event = await new Event({
     eventName,
     locationName,
@@ -24,9 +41,9 @@ const addNewEvent = wrapAsync(async (req, res) => {
     time,
     description,
     isPublic,
+    attendees: [user._id],
+    owner: user._id,
   }).save();
-  // add attendee
-  const user = await User.findById(userId);
   user.createdEvents = [...new Set([...user.createdEvents, event])];
   user.registeredEvents = [...new Set([...user.registeredEvents, event])];
   await user.save();
